2cc6c79c1aa755aaedd7c7f0be23ce49f3fa5eb1
Cecilie Klarpaas <cklarpaas@gmail.com>
2022-05-08 12:24:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div>Place a logo or something here</div>
+			</header>
+			<p>Hello</p>
+		</div>
+	);
+}
+
+export default App;
