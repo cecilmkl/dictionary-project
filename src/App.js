69d5cb1836e6cdb33e2f85f65f0a54382aac69bd
Cecilie Klarpaas <cklarpaas@gmail.com>
2022-05-08 15:35:44 +0200

    Fixed footer, README, set up for photos later

--- src/App.js
+++ src/App.js
@@ -11,7 +11,32 @@ function App() {
 				<h1>Dictionary app</h1>
 				<Dictionary />
 			</main>
-			<footer>Coded by me</footer>
+			<footer>
+				This project was coded by{" "}
+				<a
+					href="https://xenodochial-morse-ecd400.netlify.app/about.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Cecilie K.
+				</a>{" "}
+				and is{" "}
+				<a
+					href="https://github.com/cecilmkl/dictionary-project"
+					target="_blank"
+					rel="noreferrer"
+				>
+					open-sourced on GitHub
+				</a>{" "}
+				and{" "}
+				<a
+					href="https://vermillion-meerkat-bc7d22.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					hosted on Netlify.
+				</a>
+			</footer>
 		</div>
 	);
 }
--- src/Results.js
+++ src/Results.js
@@ -6,15 +6,20 @@ export default function Results(props) {
 	if (props.results) {
 		return (
 			<div className="Results">
-				<h2 className="text-capitalize">{props.results.word}</h2>
-				<h3>{props.results.phonetic}</h3>
-				{props.results.meanings.map(function (meaning, index) {
-					return (
-						<div key={index}>
-							<Meaning meaning={meaning} />
-						</div>
-					);
-				})}
+				<div className="row">
+					<div className="col-8">
+						<h2 className="text-capitalize">{props.results.word}</h2>
+						<h3>{props.results.phonetic}</h3>
+						{props.results.meanings.map(function (meaning, index) {
+							return (
+								<div key={index}>
+									<Meaning meaning={meaning} />
+								</div>
+							);
+						})}
+					</div>
+					<div className="col-4">{/* Display photos here later */}</div>
+				</div>
 			</div>
 		);
 	} else {
